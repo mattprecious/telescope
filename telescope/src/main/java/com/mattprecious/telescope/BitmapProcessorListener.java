9c89db0fcb22a2aa2ebcdffe20ce7ccde1e0bb7c
Eric Cochran <Eric@NightlyNexus.com>
2017-01-22 14:00:32 -0800

    Disallow BitmapProcessorListener implementations
    This class is public only to allow users to call onBitmapReady.

--- telescope/src/main/java/com/mattprecious/telescope/BitmapProcessorListener.java
+++ telescope/src/main/java/com/mattprecious/telescope/BitmapProcessorListener.java
@@ -7,7 +7,11 @@ import android.support.annotation.Nullable;
  * Interface definition for a callback to be invoked when additional processing on the screenshot
  * has been completed.
  */
-public interface BitmapProcessorListener {
+public abstract class BitmapProcessorListener {
   /** Called when additional processing on the screenshot has been completed. */
-  void onBitmapReady(@Nullable Bitmap screenshot);
+  public abstract void onBitmapReady(@Nullable Bitmap screenshot);
+
+  BitmapProcessorListener() {
+    // No external subclasses.
+  }
 }
