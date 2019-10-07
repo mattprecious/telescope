3928af943f5c2fc4a4f9b5475bd1c90c9ee73364
Julio Zynger <julioz92@gmail.com>
2019-10-07 09:15:23 +0200

    Post notification when service gets started

--- telescope/src/main/java/com/mattprecious/telescope/TelescopeProjectionService.java
+++ telescope/src/main/java/com/mattprecious/telescope/TelescopeProjectionService.java
@@ -1,15 +1,44 @@
 package com.mattprecious.telescope;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
-public class TelescopeProjectionService extends Service {
+import static android.os.Build.VERSION_CODES.Q;
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
+@TargetApi(Q)
+public class TelescopeProjectionService extends Service {
+    public static final String NOTIFICATION_CHANNEL_ID = "Telescope Notifications";
+    public static final int SERVICE_ID = NOTIFICATION_CHANNEL_ID.hashCode();
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannel();
+        startForeground(
+                SERVICE_ID,
+                new Notification.Builder(this, NOTIFICATION_CHANNEL_ID).build()
+        );
+    }
+
+    private void createNotificationChannel() {
+        NotificationChannel serviceChannel = new NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                "Telescope Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(serviceChannel);
+    }
+
+
+    @Nullable @Override public IBinder onBind(Intent intent) {
         return null;
     }
 
