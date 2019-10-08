3d966195525656c187697f0ec5c243f409f24ac8
Júlio Zynger <julioz92@gmail.com>
2019-10-08 17:33:07 +0200

    Run foreground service while capturing (#78)

---
+++ telescope/src/main/java/com/mattprecious/telescope/TelescopeProjectionService.java
@@ -0,0 +1,45 @@
+package com.mattprecious.telescope;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import static android.os.Build.VERSION_CODES.Q;
+
+@TargetApi(Q)
+public class TelescopeProjectionService extends Service {
+  public static final String NOTIFICATION_CHANNEL_ID = "Telescope Notifications";
+  public static final int SERVICE_ID = NOTIFICATION_CHANNEL_ID.hashCode();
+
+  @Override public void onCreate() {
+    super.onCreate();
+
+    createNotificationChannel();
+    startForeground(
+        SERVICE_ID,
+        new Notification.Builder(this, NOTIFICATION_CHANNEL_ID).build()
+    );
+  }
+
+  private void createNotificationChannel() {
+    NotificationChannel serviceChannel = new NotificationChannel(
+        NOTIFICATION_CHANNEL_ID,
+        "Telescope",
+        NotificationManager.IMPORTANCE_MIN
+    );
+
+    NotificationManager notificationManager = getSystemService(NotificationManager.class);
+    notificationManager.createNotificationChannel(serviceChannel);
+  }
+
+
+  @Nullable @Override public IBinder onBind(Intent intent) {
+    return null;
+  }
+
+}
